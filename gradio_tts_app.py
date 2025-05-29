28a3ee07a032c967a471491be8d8a989b1491307
achaiah <3967183+achaiah@users.noreply.github.com>
2025-05-29 08:37:12 -0500

    Update gradio_tts_app.py

--- gradio_tts_app.py
+++ gradio_tts_app.py
@@ -6,6 +6,7 @@ from chatterbox.tts import ChatterboxTTS
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
 # DEVICE = "cpu"
 
 def set_seed(seed: int):
